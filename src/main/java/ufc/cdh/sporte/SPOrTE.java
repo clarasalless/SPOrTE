9780cafede9b525823c433f8ee336f05ea3aadd3
clarasalless <clarasalles.95@gmail.com>
2023-06-05 12:44:39 -0300

    commit teste

---
+++ src/main/java/ufc/cdh/sporte/SPOrTE.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ufc.cdh.sporte;
+
+/**
+ *
+ * @author clara
+ */
+public class SPOrTE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
