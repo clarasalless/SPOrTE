479000380368127f19de73d30d57bcdd4b3b5a9f
clarasalless <95931755+clarasalless@users.noreply.github.com>
2023-07-09 18:18:54 -0300

    mais comentarios

--- src/main/java/ufc/cdh/sporte/controladores/ControladorTimes.java
+++ src/main/java/ufc/cdh/sporte/controladores/ControladorTimes.java
@@ -11,30 +11,36 @@ public class ControladorTimes {
 		this.times = times;
 	}
 	
+	//retorna o vetor de times
 	public VectorTimes getTimes() {
 		return this.times;
 	}
 	
+	//insere um time no vetor de times
 	public void inserirTime(TimeAbstrato time) {
 		if(!times.timeExiste(time.getNome())){
 			times.inserirTime(time);
 		}
 	}
 	
+	//remove um time do vetor de times
 	public void removerTime(TimeAbstrato time) {
 		if(times.timeExiste(time.getNome())){
 			times.removerTime(time);
 		}
 	}
 	
+	//busca um time pela posição no vetor
 	public TimeAbstrato buscaTime(int i) {
 		return times.buscaTime(i);
 	}
 	
+	//utiliza o nome do time para verificar se ele ja foi cadstrado	
 	public boolean timeExiste(String nome) {
 		return times.timeExiste(nome);
 	}
 	
+	//embaralha os times no vetor
 	public Vector<TimeAbstrato> embaralhaTimes(){
 		return times.embaralhaTimes();
 	}
@@ -43,6 +49,7 @@ public class ControladorTimes {
 		times.ordenaTimes(times.getTimes());
 	}
 	
+	//ordena os times do vetor pelo seed
 	public int numeroDeTimes(){
 		return times.numeroDeTimes();
 	}	
